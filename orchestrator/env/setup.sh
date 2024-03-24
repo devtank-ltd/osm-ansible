8d0dd95e27c07cd46a0c2f05db09cc08377d4fe5
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-03-24 21:55:00 +0000

    Increament automated setting up OSM-Host QEmu.

--- orchestrator/env/setup.sh
+++ orchestrator/env/setup.sh
@@ -5,7 +5,7 @@
 # debconf-get-selections --installer"
 
 DEBISO=debian-12.5.0-amd64-netinst.iso
-DEBDISK=disk.img
+DEBDISK=disk.qcow
 DEBBIOSMEM=ovmf_vars.fd
 
 mkdir -p mnt
@@ -14,11 +14,15 @@ mkdir -p boot
 cp -r mnt/install.amd boot/
 umount mnt
 
+gunzip boot/install.amd/initrd.gz
+echo preseed.cfg | cpio -H newc -o -A -F boot/install.amd/initrd
+gzip boot/install.amd/initrd
+
 # apt install ovmf qemu-system-x86
 
 dd if=/dev/zero of="$DEBBIOSMEM" bs=131072 count=1
 
-qemu-img create "$DEBDISK" 16G
+qemu-img create -f qcow2 "$DEBDISK" 16G
 
 qemu-system-x86_64                 \
    -enable-kvm                     \
@@ -29,7 +33,7 @@ qemu-system-x86_64                 \
    -device virtio-serial-pci       \
    -nic user,model=virtio-net-pci \
    -drive file="$DEBISO",format=raw,if=virtio,media=cdrom \
-   -drive file="$DEBDISK",format=raw,if=virtio \
+   -drive file="$DEBDISK",format=qcow2,if=virtio \
    -drive "if=pflash,format=raw,unit=0,file=/usr/share/OVMF/OVMF_CODE_4M.fd,readonly=on" \
    -drive "if=pflash,format=raw,unit=1,file=$DEBBIOSMEM" \
    -kernel boot/install.amd/vmlinuz \
