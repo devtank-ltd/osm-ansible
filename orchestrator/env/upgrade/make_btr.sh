ed15a8dc26f1f037852e685380a66051f225dace
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-01 23:29:34 +0100

    Fixs from testing making ram disk.

--- orchestrator/env/upgrade/make_btr.sh
+++ orchestrator/env/upgrade/make_btr.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
-set -- $(df /srv -T | awk '/dev/ { print $1,$2 }')
-srv_dev=$1
-srv_fs=$2
-root_dev=$(df / -T | awk '/dev/ { print $1,$2 }')
-boot_dev=$(df /boot -T | awk '/dev/ { print $1,$2 }')
-efi_dev=$(df /boot/efi -T | awk '/dev/ { print $1,$2 }')
+set -- $(df / -T | awk '/dev/ { print $1,$2 }')
+root_dev=$1
+root_fs=$2
+srv_dev=$(df /srv -T | awk '/dev/ { print $1 }')
+
+[ -z "$srv_dev" -o "$root_dev" == "$srv_dev" ] || (echo "/srv on own device" && exit -1)
+
+boot_dev=$(df /boot -T | awk '/dev/ { print $1 }')
+efi_dev=$(df /boot/efi -T | awk '/dev/ { print $1 }')
 
 [ "${root_fs#btrfs}" == "${root_fs}" ] || (echo "Already btrfs." && exit 0)
-[ "${srv_fs#ext}" != "${srv_fs}" ] || (echo "Unsupported rootfs to start with." && exit -1)
-[ "$root_dev" == "$srv_dev" ] || (echo "/srv on own device" && exit -1)
+[ "${root_fs#ext}" != "${root_fs}" ] || (echo "Unsupported rootfs to start with." && exit -1)
+
+mkdir -p base/etc
 
 echo "Warning, going to make ${root_dev} btrfs"
 echo "root_dev=$root_dev" > base/etc/disks
