69e0f87969a006813f74c19c7b03f62d44d1363d
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-10 12:14:35 +0100

    Add running on a dedicated NAT.

---
+++ orchestrator/env/net_ctrl.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+set -e
+
+main_ip=$(ip route | awk '/default/ { print $9 ; exit}')
+
+case "$1" in
+  "open")
+    [ ! -e /sys/class/net/vosmhostnet ] || { echo "vosmhostnet already open."; exit -1; }
+
+    [ $(id -u) == 0 ] || exec sudo -- "$0" "$@"
+  
+    [ -d /etc/qemu ] || mkdir /etc/qemu
+    [ -e /etc/qemu/bridge.conf ] || { touch /etc/qemu/bridge.conf; chmod 640 /etc/qemu/bridge.conf; chown root:netdev /etc/qemu/bridge.conf; }
+    [ -n "$(grep vosmhostnet /etc/qemu/bridge.conf)" ] || echo "allow vosmhostnet" >> /etc/qemu/bridge.conf
+
+    ip link add vosmhostnet type bridge
+    ip addr add 192.168.5.1/24 dev vosmhostnet
+    ip link set up dev vosmhostnet
+
+    iptables -t nat -A POSTROUTING -s 192.168.5.0/24 -j SNAT --to-source $main_ip
+
+    dnsmasq --pid-file=/tmp/vosmhostnet.pid --interface=vosmhostnet --bind-interfaces --dhcp-range=192.168.5.2,192.168.5.255
+  ;;
+  "close")
+    [ -e /sys/class/net/vosmhostnet ] || { echo "vosmhostnet already closed."; exit -1; }
+
+    [ $(id -u) == 0 ] || exec sudo -- "$0" "$@"
+
+    iptables -t nat -D POSTROUTING -s 192.168.5.0/24 -j SNAT --to-source $main_ip
+
+    kill -9 $(cat /tmp/vosmhostnet.pid)
+
+    ip link set down dev vosmhostnet
+    ip addr del 192.168.5.1/24 dev vosmhostnet
+    ip link del vosmhostnet type bridge
+  ;;
+  *)
+    echo "Unknown operation, options are: open, close"
+  ;;
+esac
--- orchestrator/env/run.sh
+++ orchestrator/env/run.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+./net_ctrl.sh open
 
 if [ -z "$DEBDISK" ]; then DEBDISK=disk.qcow; fi
 DEBBIOSMEM=ovmf_vars.fd
@@ -11,7 +12,7 @@ qemu-system-x86_64                 \
    -m 4G                           \
    -device virtio-scsi-pci,id=scsi \
    -device virtio-serial-pci       \
-   -nic user,model=virtio-net-pci \
+   -nic bridge,br=vosmhostnet,model=virtio-net-pci \
    -drive file="$DEBDISK",format=qcow2,if=virtio \
    -drive "if=pflash,format=raw,unit=0,file=/usr/share/OVMF/OVMF_CODE_4M.fd,readonly=on" \
    -drive "if=pflash,format=raw,unit=1,file=$DEBBIOSMEM"
