7f8fd61a63e410df640d4b6f8c6fb8631760f892
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-10 21:14:33 +0100

    Fixup setting hostname and don't try postinstall in preseed.

--- orchestrator/env/setup.sh
+++ orchestrator/env/setup.sh
@@ -70,7 +70,7 @@ qemu-system-x86_64                 \
    -m 4G                           \
    -device virtio-scsi-pci,id=scsi \
    -device virtio-serial-pci       \
-   -nic user,model=virtio-net-pci \
+   -nic user,model=virtio-net-pci,hostname=osmhost \
    -drive file="$DEBISO",format=raw,if=virtio,media=cdrom \
    -drive file="$DEBDISK",format=qcow2,if=virtio \
    -drive "if=pflash,format=raw,unit=0,file=/usr/share/OVMF/OVMF_CODE_4M.fd,readonly=on" \
