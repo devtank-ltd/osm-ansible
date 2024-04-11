4c641fc2fba25466d787895329e847d2f46c9c8e
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-11 23:48:27 +0100

    Fixes from testing.

--- orchestrator/env/raw.postinstall.sh
+++ orchestrator/env/raw.postinstall.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
-wget http://IPADDR:8000/osmhost_setup.sh -O /root/osmhost_setup.sh;
-chmod +x /root/osmhost_setup.sh;
-
+# Setup SSH access
 ssh_key_name=$(wget http://IPADDR:8000/ssh_key_name -O -)
 mkdir -p /root/.ssh
 wget http://IPADDR:8000/$ssh_key_name -O - >> /root/.ssh/authorized_keys
 
+# Fake up LetsEncrypt
 mkdir -p /etc/letsencrypt/live/OSMHOST.osmm.devtank.co.uk
+openssl req -x509 -nodes -newkey rsa:4096 -days 1\
+    -keyout "/etc/letsencrypt/live/OSM_HOST_NAME.osmm.devtank.co.uk/privkey.pem" \
+    -out "/etc/letsencrypt/live/OSM_HOST_NAME.osmm.devtank.co.uk/fullchain.pem" \
+    -subj '/CN=localhost'
+
+echo "export SKIP_LETS_ENCRYPT=1" >> /root/.bashrc
