0c832ba513d1c91bacab713401137262bb77f161
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-12 14:22:44 +0100

    Add argument protection to script.

--- ansible/do-create-container.sh
+++ ansible/do-create-container.sh
@@ -8,6 +8,9 @@ owndir=$(dirname $path)
 customer_name=$1
 mqtt_port=$2
 
+[ -n "$customer_name" ] || { echo "No customer name given."; exit -1; }
+[ -n "$mqtt_port" ] || { echo "No MQTT port given."; exit -1; }
+
 cd "$owndir"
 
 git pull
