df711af9065c8d8fde091ea225f2ef355fd8c9e3
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-18 16:49:37 +0100

    First draft of script to setup a OSM Orchestrator VM.

---
+++ orchestrator/env/setup_orchestrator.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+PRESEED=preseed-ext4.cfg
+
+[ -n "$OSMHOST" ] || OSMHOST=orchestrator
+
+. setup_common.sh
+
+ansible-playbook -v -e "target=$vm_ip osm_host_name=$OSMHOST osm_domain=$OSM_DOMAIN osm_dns=$vm_ip " -i "$ANSIBLE_HOSTS" osmhost_orchestrator.yaml
+
+[ -n "$POWER_ON" ] || ssh root@$vm_ip "poweroff"
