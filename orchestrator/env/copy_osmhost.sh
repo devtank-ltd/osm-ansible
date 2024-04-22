8457f54433e4db26934fcac2bc29fe25709023f2
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-22 18:52:04 +0100

    Add script to clone a OSM Host.

---
+++ orchestrator/env/copy_osmhost.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+src=$1
+dst=$2
+
+HOSTS_DIR=hosts
+
+if [ ! -e "$HOSTS_DIR/$src" ]
+then
+  echo "Source host "$src" does not exist"
+  exit -1
+fi
+
+mkdir "$HOSTS_DIR/$dst"
+
+cp "$HOSTS_DIR/$src/"{disk.qcow,ovmf_vars.fd} "$HOSTS_DIR/$dst/"
+
+OSMHOST=$dst
+
+. async_run.sh
+
+ssh root@$vm_ip "sed -i \"s|$src|$dst|g\" /etc/{hosts,hostname}"
+ssh root@$vm_ip "poweroff"
