a5b400c1e51383349e12a21127f879ecc5004749
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-22 22:17:47 +0100

    Don't stdout ping.

--- orchestrator/env/get_active_ip_of_mac.sh
+++ orchestrator/env/get_active_ip_of_mac.sh
@@ -8,7 +8,7 @@ mac_addres=$2
 vm_ip=$(awk "/$mac_addres/ {print \$3}" "/tmp/$bridge_name.leasefile")
 if [ -n "$vm_ip" ]
 then
-  ping -c1 -W1 $vm_ip
+  ping -c1 -W1 $vm_ip >/dev/null
   if [ "$?" == "0" ]
   then
     echo $vm_ip
