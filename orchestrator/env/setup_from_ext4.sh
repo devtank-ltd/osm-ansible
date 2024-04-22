debccf5ca38219fe196f4533532a15e225bd1860
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-22 19:38:54 +0100

    Add a bit more verbose messages for setup scripts.

--- orchestrator/env/setup_from_ext4.sh
+++ orchestrator/env/setup_from_ext4.sh
@@ -6,6 +6,8 @@ PRESEED=preseed-ext4.cfg
 
 . setup_common.sh
 
+echo "Applying From Ext4 OSM Host Ansible"
+
 ansible-playbook -v -e "target=$vm_ip osm_host_name=$OSMHOST osm_domain=$OSM_DOMAIN fake_osm_host=True" -i "$ANSIBLE_HOSTS" osmhost_from_ext4.yaml
 
 [ -n "$POWER_ON" ] || ssh root@$vm_ip "poweroff"
--- orchestrator/env/setup_orchestrator.sh
+++ orchestrator/env/setup_orchestrator.sh
@@ -6,6 +6,8 @@ PRESEED=preseed-ext4.cfg
 
 . setup_common.sh
 
+echo "Applying Orchestrator Ansible"
+
 ansible-playbook -v -e "target=$vm_ip osm_host_name=$OSMHOST osm_domain=$OSM_DOMAIN osm_dns=$vm_ip " -i "$ANSIBLE_HOSTS" osmhost_orchestrator.yaml
 
 [ -n "$POWER_ON" ] || ssh root@$vm_ip "poweroff"
