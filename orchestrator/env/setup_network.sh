d26762e984fc7208bc6f8c28b7820b46237b2839
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-23 16:52:55 +0100

    Add fake OSM HOSTs to fake OSM Orchestrator. Add fake customer to fake system.

--- orchestrator/env/setup_network.sh
+++ orchestrator/env/setup_network.sh
@@ -2,6 +2,8 @@
 
 [ -n "$VOSMHOSTBR" ] || VOSMHOSTBR=vosmhostbr0
 [ -n "$HOSTS_DIR" ] || HOSTS_DIR=hosts
+[ -n "$OSMHOST_COUNT" ] || OSMHOST_COUNT=3
+[ -n "$OSMCUSTOMER_COUNT" ] || OSMHOST_COUNT=10
 
 echo "Creating OSM Orchestrator"
 ./setup_orchestrator.sh
@@ -10,7 +12,6 @@ echo "========================================="
 echo "Creating first virtual OSMHOST"
 OSMHOST=vosmhost0 ./setup_from_btrfs.sh
 
-[ -n "$OSMHOST_COUNT" ] || OSMHOST_COUNT=3
 
 echo "========================================="
 echo "Cloning $(($OSMHOST_COUNT - 1)) virtual OSMHOSTS"
@@ -25,7 +26,22 @@ done
 echo "========================================="
 echo "Network started, adding OSM HOSTs to Orchestrator"
 
+ssh root@$orchestrator_ip "ssh-keygen -q  -t rsa -N '' -f /root/.ssh/id_rsa"
+orchestrator_pub=$(ssh root@$orchestrator_ip "cat /root/.ssh/id_rsa.pub")
+
 for n in `seq 0 $OSMHOST_COUNT`
 do
-  ssh root@$orchestrator_ip "/srv/osm-lxc/orchestrator/orchestrator_cli.py add_host vosmhost$n ${host_ip[$n]} 4"
+  name=vosmhost$n
+  ip_addr=${host_ip[$n]}
+  ssh root@$ip_addr 'mkdir -p /home/osm_orchestrator/.ssh'
+  ssh root@$ip_addr "echo $orchestrator_pub >> /home/osm_orchestrator/.ssh/authorized_keys"
+  ssh root@$orchestrator_ip "ssh-keyscan -H $ip_addr >> /root/.ssh/known_hosts"
+  ssh root@$orchestrator_ip "/srv/osm-lxc/orchestrator/orchestrator_cli.py add_host $name $ip_addr 4"
+done
+
+
+for n in `seq $OSMCUSTOMER_COUNT`
+do
+  customer_name="customer_$n"
+  ssh root@$orchestrator_ip "/srv/osm-lxc/orchestrator/orchestrator_cli.py add_customer $customer_name"
 done
