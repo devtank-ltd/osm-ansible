4763060e72ca64bb4259bc83cd6a8295288ed1db
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-23 12:27:20 +0100

    Fixes from testing.

--- orchestrator/env/run_network.sh
+++ orchestrator/env/run_network.sh
@@ -16,7 +16,7 @@ do
   echo "Starting OSM HOST: vosmhost$n"
   OSMHOST="vosmhost$n" ./run.sh&
   host_pid[$n]=$!
-  host_mac[$n]=$(cat $HOSTS_DIR/vosmhost1/mac)
+  host_mac[$n]=$(cat $HOSTS_DIR/vosmhost$n/mac)
   echo "OSM HOST: vosmhost$n  PID:${host_pid[$n]} MAC:${host_mac[$n]}"
   sleep 0.1 # Give it a little time to acturally start.
 done
@@ -25,16 +25,20 @@ echo "========================================="
 echo "Waiting on network"
 
 count=0
-while [ -z "$orchestrator_ip" -a $count = $OSMHOST_COUNT ]
+while [ -z "$orchestrator_ip" -o $count = $OSMHOST_COUNT ]
 do
   sleep 0.25
-  [ -n "$orchestrator_ip" ] || orchestrator_ip=$(./get_active_ip_of_mac.sh $VOSMHOSTBR $orchestrator_mac)
+  if [ -z "$orchestrator_ip" ]
+  then
+    orchestrator_ip=$(./get_active_ip_of_mac.sh $VOSMHOSTBR $orchestrator_mac)
+    [ -z "$orchestrator_ip" ] || echo "Orchestrator : $orchestrator_ip"
+  fi
   for n in `seq 0 $OSMHOST_COUNT`
   do
     if [ -z "${host_ip[$n]}" ]
     then
       ip_addr=$(./get_active_ip_of_mac.sh $VOSMHOSTBR ${host_mac[$n]})
-      [ -z "$ip_addr" ] || { echo "$name : $ip_addr"; host_ip[$n]=$ip_addr; }
+      [ -z "$ip_addr" ] || { echo "vosmhost$n : $ip_addr"; host_ip[$n]=$ip_addr; }
     fi
   done
   count=0
