365c5048e9e36a35d3485dea383022f1c4543254
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-24 23:00:47 +0100

    Wait for vm to exit.

--- orchestrator/env/setup_from_btrfs.sh
+++ orchestrator/env/setup_from_btrfs.sh
@@ -6,4 +6,4 @@ echo "Applying OSM Host Ansible"
 
 ansible-playbook -e "target=$vm_ip osm_host_name=$OSMHOST osm_domain=$OSM_DOMAIN fake_osm_host=True" -i "$ANSIBLE_HOSTS" osmhost_from_btrfs.yaml
 
-[ -n "$POWER_ON" ] || ssh root@$vm_ip "poweroff"
+[ -n "$POWER_ON" ] || { ssh root@$vm_ip "poweroff"; while [ -e /proc/$run_pid ]; do sleep 0.25; done; }
