ca3e8061b035d15290db4b9f5fd77316308e181f
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-04-30 12:21:15 +0100

    First draft of setting up DNS of fake network.

--- orchestrator/env/setup_from_ext4.sh
+++ orchestrator/env/setup_from_ext4.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+[ -n "$OSMORCHESTRATOR" ] || { echo "No Orchestrator set."; exit -1; }
+
 PRESEED=preseed-ext4.cfg
 
 [ -n "$OSMHOST" ] || OSMHOST=ext4deb
@@ -8,6 +10,6 @@ PRESEED=preseed-ext4.cfg
 
 echo "Applying From Ext4 OSM Host Ansible"
 
-ansible-playbook -v -e "target=$vm_ip osm_host_name=$OSMHOST osm_domain=$OSM_DOMAIN fake_osm_host=True" -i "$ANSIBLE_HOSTS" osmhost_from_ext4.yaml
+ansible-playbook -v -e "target=$vm_ip osm_orchestrator=$OSMORCHESTRATOR osm_host_name=$OSMHOST osm_domain=$OSM_DOMAIN fake_osm_host=True" -i "$ANSIBLE_HOSTS" osmhost_from_ext4.yaml
 
 [ -n "$POWER_ON" ] || { ssh root@$vm_ip "poweroff"; wait $run_pid; }
