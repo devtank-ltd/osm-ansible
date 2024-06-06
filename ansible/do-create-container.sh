f71c92b62b77899affe4949946fadf0770d3c67a
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-06-06 19:02:28 +0100

    First draft of domain changes to allow overloading.

--- ansible/do-create-container.sh
+++ ansible/do-create-container.sh
@@ -7,18 +7,21 @@ owndir=$(dirname $path)
 
 customer_name=$1
 mqtt_port=$2
+domain=$3
 
 [ -n "$customer_name" ] || { echo "No customer name given."; exit -1; }
 [ -n "$mqtt_port" ] || { echo "No MQTT port given."; exit -1; }
 
 cd "$owndir"
 
+[ -e hosts ] || { echo "localhost" > hosts; }
+
 git pull
 
-domain=$(ls /etc/letsencrypt/live/ | grep -v README | head -n 1 | awk -F '.' 'BEGIN { OFS="."}; {$1=""; print $0}')
+[ -z "$domain" ] || { extra="$extra le_domain=$domain"; echo "Using custom domain $domain, make sure to use it on delete."; }
 
-ansible-playbook -i hosts -e "customer_name=$customer_name mqtt_port=$mqtt_port" create-container.yaml
+ansible-playbook -i hosts -e "customer_name=$customer_name mqtt_port=$mqtt_port $extra" create-container.yaml
 
 echo $customer_name-svr >> hosts
 
-ansible-playbook -i hosts -e "target=$customer_name-svr customer_name=$customer_name base_domain=$customer_name$domain" provision-container.yaml
+ansible-playbook -i hosts -e "target=$customer_name-svr customer_name=$customer_name $extra" provision-container.yaml
--- ansible/do-delete-container.sh
+++ ansible/do-delete-container.sh
@@ -7,11 +7,14 @@ owndir=$(dirname $path)
 
 customer_name=$1
 mqtt_port=$2
+domain=$3
 
 cd "$owndir"
 
 git pull
 
-ansible-playbook -i hosts -e "customer_name=$customer_name" delete-container.yaml
+[ -z "$domain" ] || { extra="$extra le_domain=$domain"; echo "Using custom domain $domain."; }
+
+ansible-playbook -i hosts -e "customer_name=$customer_name $extra" delete-container.yaml
 
 sed -i "/$customer_name-svr/d" hosts
