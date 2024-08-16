1f6656f6b9ceb1b1b81ff223cf350c2a7636d359
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-08-16 12:47:46 +0100

    Move Ansible to upgrade ext4 to btrfs.

--- orchestrator/env/test_ext4_upgrading.sh
+++ orchestrator/env/test_ext4_upgrading.sh
@@ -6,8 +6,8 @@ source setup_start.sh
 
 echo "Upgrade ext4 install to btrfs install"
 PRESEED=preseed-ext4.cfg
-ansible_file=ext4_to_btrfs.yaml
+ansible_file=upgrade/ext4_to_btrfs.yaml
 
 source setup_common.sh
 
-ansible-playbook -v -e "target=$vm_ip osm_host_name=$OSM_HOST osm_domain=$OSM_DOMAIN fake_osm_host=True" -i "$ANSIBLE_HOSTS" setup_from_btrfs.yaml
+ansible-playbook -v -e "target=$vm_ip osm_host_name=$OSM_HOST osm_domain=$OSM_DOMAIN fake_osm_host=True" -i "$ANSIBLE_HOSTS" osmhost_from_btrfs.yaml
