7605bea9ab1dadb347710b9cb86789da0b274e35
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-08-16 14:41:42 +0100

    Fix up run state in upgrade test.

--- orchestrator/env/test_ext4_upgrading.sh
+++ orchestrator/env/test_ext4_upgrading.sh
@@ -8,6 +8,11 @@ echo "Upgrade ext4 install to btrfs install"
 PRESEED=preseed-ext4.cfg
 ansible_file=upgrade/ext4_to_btrfs.yaml
 
+export POWER_ON=1 # Don't turn off after first ansible run, as we are going again.
+
 source setup_common.sh
 
 ansible-playbook -v -e "target=$vm_ip osm_host_name=$OSM_HOST osm_domain=$OSM_DOMAIN fake_osm_host=True" -i "$ANSIBLE_HOSTS" osmhost_from_btrfs.yaml
+
+ssh root@$vm_ip "poweroff"
+wait $run_pid
