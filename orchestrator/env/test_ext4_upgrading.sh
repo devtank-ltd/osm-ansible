4f19be99e7007b101f5655a870950875af381678
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-10-07 11:25:52 +0100

    When creating test env, use current branch for clones.

--- orchestrator/env/test_ext4_upgrading.sh
+++ orchestrator/env/test_ext4_upgrading.sh
@@ -12,7 +12,7 @@ export POWER_ON=1 # Don't turn off after first ansible run, as we are going agai
 
 source setup_common.sh
 
-ansible-playbook -v -e "target=$vm_ip osm_host_name=$OSM_HOST osm_domain=$OSM_DOMAIN fake_osm_host=True" -i "$ANSIBLE_HOSTS" osmhost_from_btrfs.yaml
+ansible-playbook -v -e "target=$vm_ip osm_host_name=$OSM_HOST osm_domain=$OSM_DOMAIN fake_osm_host=True osm_ansible_branch=$git_branch" -i "$ANSIBLE_HOSTS" osmhost_from_btrfs.yaml
 
 ssh root@$vm_ip "poweroff"
 wait $run_pid
