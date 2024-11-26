dad6dd9d1bf870b68d983a63ce4d9be45fce130b
Marcus Holder <marcus.holder@devtank.co.uk>
2024-11-26 16:25:28 +0000

    Amend influx inserter script and add prometheus config to template yaml.

--- ansible/do-influx-inserter-upgrade.bash
+++ ansible/do-influx-inserter-upgrade.bash
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+set -o errexit                  # exit when command fails
+set -o nounset                  # error when expanding unset var
+set -o pipefail                 # do not hide error(s) in pipes
+
+readonly DEVTANK_DIR="/srv/osm-lxc"
+readonly CONTAINERS_PATH="${DEVTANK_DIR}/lxc/containers"
+
 die() {
     echo "$*" >&2
     exit 1
@@ -11,15 +18,9 @@ container_name() {
 }
 
 lxc_do() {
-    # execute "cmd" command in lxc container "name"
     local name="$1"; shift
     local cmd=( "$@" )
     local config="${CONTAINERS_PATH}/${name}/lxc.container.conf"
-
-
-    if [[ "$name" == "$BASE_CONTAINER" ]]; then
-        config="/srv/osm-lxc/lxc/os-bases/base-os-lxc.conf"
-    fi
 
     lxc-attach -n "$name" \
                -f "$config" \
@@ -30,18 +31,26 @@ lxc_do() {
 main() {
     local name="$1"
     local container_name
+    local backup="/root/mqtt-influx-inserter.yaml.bkup"
+    local target="/etc/mqtt-influx-inserter/mqtt-influx-inserter.yaml"
+    local content='''
+    prometheus:
+      orch_wg_ip: 10.10.1.1
+      push_gateway_port: 9091
+    '''
 
     [[ -z "$name" ]] && die "No container name is given"
     printf -v container_name '%s' "$(container_name "$name")"
     [[ ! -v container_name ]] && die "The container '$container_name' does not exist"
 
-    lxc_do "$container_name" cp /etc/mqtt-influx-inserter/mqtt-influx-inserter.yaml /tmp/mqtt-influx-inserter.yaml.bkup
-    lxc_do "$container_name" apt purge mqtt-influx-inserter
+    lxc_do "$container_name" mv "$target" "$backup"
+    lxc_do "$container_name" apt purge mqtt-influx-inserter -y
     lxc_do "$container_name" deluser --system influx-inserter
     lxc_do "$container_name" mkdir -p /tmp/debs/upgr
     lxc_do "$container_name" wget https://cloud.devtank.co.uk/debs/mqtt-influx-inserter_0.1.1-1_all.deb -P /tmp/debs/upgr
     lxc_do "$container_name" apt install /tmp/debs/upgr/mqtt-influx-inserter_0.1.1-1_all.deb
-    lxc_do "$container_name" mv /tmp/mqtt-influx-inserter.yaml.bkup /etc/mqtt-influx-inserter/mqtt-influx-inserter.yaml
+    lxc_do "$container_name" echo "$content" >> "$backup"
+    lxc_do "$container_name" mv "$backup" "$target"
     lxc_do "$container_name" systemctl restart mqtt-influx-inserter
 }
 
